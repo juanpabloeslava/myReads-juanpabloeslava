c630739d2f039a5bef59d3c87b9988abbded3d05
Juan Pablo Eslava <jpesmar@gmail.com>
2018-11-19 10:16:43 +0100

    JP - feat: added BrowserRouter to manage Views rendering

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
 import React from 'react'
+import { Route } from 'react-router-dom';
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
 import Home from './views/Home';
 import Search from './views/Search';
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  }
 
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <Search />      
-        ) : (
-          <Home />
-        )}
+        <Route exact path="/" render={ () => (
+          <Home/>
+        )}/>
+        <Route exact path="/search" render={ () => (
+          <Search/>
+        )}/>
       </div>
     )
   }
