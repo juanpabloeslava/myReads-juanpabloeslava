dbf2155f4255d9dce4ebc9a02491a684cd57e229
Juan Pablo Eslava <jpesmar@gmail.com>
2018-11-21 17:22:33 +0100

    JP - fix: change app.js

--- src/App.js
+++ src/App.js
@@ -1,39 +1,20 @@
 import React from 'react'
-import { Route, Switch } from 'react-router-dom';
-import Provider, { JpContext } from './provider/indexProv';
-// components
+import { Route } from 'react-router-dom';
+import './App.css'
 import Home from './views/Home';
 import Search from './views/Search';
-// css
-import './App.css'
 
 class BooksApp extends React.Component {
 
   render() {
     return (
       <div className="app">
-        <Provider>
-          <Switch>
-            <Route 
-              exact path="/" 
-              render={() => (
-                // Subscribe Home as a consumer of the JpContext provider (from the ./provider/indexProv.js). It requires a func as a child
-                <JpContext.Consumer>
-                  { context => <Home { ...context }/> }
-                </JpContext.Consumer>
-              )} 
-            />
-            <Route 
-              exact path="/search" 
-              render={() => (
-                // Subscribe Search as a consumer of the JpContext provider (from the ./provider/indexProv.js). It requires a func as a child
-                <JpContext.Consumer>
-                  { context => <Search { ...context }/> }
-                </JpContext.Consumer>
-              )} 
-            />
-          </Switch>  
-        </Provider>
+        <Route exact path="/" render={ () => (
+          <Home/>
+        )}/>
+        <Route exact path="/search" render={ () => (
+          <Search/>
+        )}/>
       </div>
     )
   }
